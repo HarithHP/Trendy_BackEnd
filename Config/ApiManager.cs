596986764b51c1e003aa7dcd45f335b4cb245888
HarithHp <hphadaragama@gmail.com>
2024-03-24 20:44:20 +0530

    Add project files.

---
+++ Config/ApiManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Data.SqlClient;
+using static Trendy_BackEnd.Config.EnumCollection;
+
+namespace Trendy_BackEnd.Config
+{
+    public class ApiManager
+    {
+        private readonly string Environment = TrendyEnvironment.Development.ToString();
+        private readonly string AuthToken = "IOSApplication";
+
+        private ApiManager() { }
+        private static ApiManager instance = null;
+        internal static ApiManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ApiManager();
+                }
+                return instance;
+            }
+        }
+
+        internal SqlConnectionStringBuilder GetConnectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            if (Environment == TrendyEnvironment.Development.ToString())
+            {
+                builder.DataSource = "SQL5106.site4now.net";
+                builder.InitialCatalog = "db_aa650d_harith98";
+                builder.UserID = "db_aa650d_harith98_admin";
+                builder.Password = "Hph@harith1234";
+            }
+            else if (Environment == TrendyEnvironment.UAT.ToString())
+            {
+                builder.DataSource = "";
+                builder.InitialCatalog = "";
+                builder.UserID = "";
+                builder.Password = "";
+            }
+            else if (Environment == TrendyEnvironment.Live.ToString())
+            {
+                builder.DataSource = "";
+                builder.InitialCatalog = "";
+                builder.UserID = "";
+                builder.Password = "";
+            }
+            return builder;
+        }
+
+        internal string GetEnvironment()
+        { return Environment; }
+    }
+}
---
+++ Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Trendy_BackEnd.Config.EnumCollection;
+using System.ComponentModel.DataAnnotations;
+using Trendy_BackEnd.Models;
+using Trendy_BackEnd.Services;
+
+namespace Trendy_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+
+        //Fixed custom authentication
+        [HttpGet]
+        [Route("Login")]
+        public async Task<ResponseResult> SignIn([FromQuery, Required] string Email, [FromQuery, Required] string Password)
+        {
+            if (string.IsNullOrWhiteSpace(Email) || (string.IsNullOrWhiteSpace(Password)))
+            {
+                return new ResponseResult()
+                {
+                    Status = ApiRespond.Fail.ToString(),
+                    Content = null,
+                    Message = "Email or Password cannot be empty , Please Recheck your Inputs"
+                };
+            }
+            else
+            {
+                AuthServices Service = new();
+                return await Service.SignIn(Email, Password);
+            }
+
+        }
+
+        //Fixed custom authentication
+        [HttpPost]
+        [Route("SignUp")]
+        public async Task<ResponseResult> SignUp([FromQuery, Required] ModelUser model)
+        {
+            if (string.IsNullOrWhiteSpace(model.Email) || (string.IsNullOrWhiteSpace(model.Password)) || string.IsNullOrWhiteSpace(model.Address1) || string.IsNullOrWhiteSpace(model.FirstName) || string.IsNullOrWhiteSpace(model.LastName) || string.IsNullOrWhiteSpace(model.ContactNumber))
+            {
+                return new ResponseResult()
+                {
+                    Status = ApiRespond.Fail.ToString(),
+                    Content = null,
+                    Message = "Email | Password | Address1 | First Name | Last Name | Contact Number cannot be empty , Please Recheck your Inputs"
+                };
+            }
+            else
+            {
+                AuthServices Service = new();
+                return await Service.SignUp(model);
+            }
+
+        }
+
+    }
+}
